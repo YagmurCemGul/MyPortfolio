03d55257ad06266b9c0dab4736266d4378f92a82
crybf <33093636+crybf@users.noreply.github.com>
2025-09-04 07:29:16 +0300

    fix(pages): wrap with DetailModal + Portal providers in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import type { AppProps } from "next/app";
 import ReduxProvider from "src/app/ReduxProvider";
+import DetailModalProvider from "src/providers/DetailModalProvider";
+import PortalProvider from "src/providers/PortalProvider";
+import DetailModal from "src/components/DetailModal";
+import VideoPortalContainer from "src/components/VideoPortalContainer";
 import "src/app/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ReduxProvider>
-      <Component {...pageProps} />
+      <DetailModalProvider>
+        <PortalProvider>
+          {/* Global modal + hover portal for pages/ router */}
+          <DetailModal />
+          <VideoPortalContainer />
+          <Component {...pageProps} />
+        </PortalProvider>
+      </DetailModalProvider>
     </ReduxProvider>
   );
 }
+
 
