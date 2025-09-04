d87867077be44bdd85e384fc12ee925d8b4fef25
crybf <33093636+crybf@users.noreply.github.com>
2025-09-04 07:38:23 +0300

    fix(pages): add SoundProvider around pages app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,21 +4,23 @@ import DetailModalProvider from "src/providers/DetailModalProvider";
 import PortalProvider from "src/providers/PortalProvider";
 import DetailModal from "src/components/DetailModal";
 import VideoPortalContainer from "src/components/VideoPortalContainer";
+import SoundProvider from "src/providers/SoundProvider";
 import "src/app/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ReduxProvider>
-      <DetailModalProvider>
-        <PortalProvider>
-          {/* Global modal + hover portal for pages/ router */}
-          <DetailModal />
-          <VideoPortalContainer />
-          <Component {...pageProps} />
-        </PortalProvider>
-      </DetailModalProvider>
+      <SoundProvider>
+        <DetailModalProvider>
+          <PortalProvider>
+            {/* Global modal + hover portal for pages/ router */}
+            <DetailModal />
+            <VideoPortalContainer />
+            <Component {...pageProps} />
+          </PortalProvider>
+        </DetailModalProvider>
+      </SoundProvider>
     </ReduxProvider>
   );
 }
-
 
